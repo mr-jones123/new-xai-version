8cc0c081f920edefb4d53a2baa7e3c3adb74cab5
Aiverson <johnaiversonabong@gmail.com>
2025-02-19 18:47:39 +0800

    Update XeeAI Logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,13 +11,13 @@ const NavBar = () => {
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <Link
           href="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
+          className="flex items-center space-x-2 rtl:space-x-reverse"
         >
           <Image
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="/XeeAI Logo (Draft).svg"
             className="h-8"
-            width={20}
-            height={20}
+            width={30}
+            height={30}
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
