902ff742478aa9bcbb467a111a933202642981ac
Xynil Jhed Lacap <lacapxyniljhed@gmail.com>
2025-03-08 00:23:29 +0800

    feat: svg masked effect now has lime output underneath the highlighted text

--- src/components/MaskedEffect.tsx
+++ src/components/MaskedEffect.tsx
@@ -12,7 +12,8 @@ export default function MaskedEffect() {
         }
         className="h-[40rem] w-full"
       >
-        <span className="bg-blue-600 text-white p-2">XeeAI</span> let&apos;s you see under hood of <span className="bg-blue-600 text-white p-2">AI.</span>
+        <span className="bg-blue-600 text-white p-2 relative">XeeAI <span className="absolute -bottom-1 left-0 right-0 h-1 font-geist text-[1.5rem]">0.69</span></span> 
+        let&apos;s you see under hood of <span className="bg-blue-400 text-white p-2 relative">AI. <span className="absolute -bottom-1  left-0 right-0 h-1 font-geist text-[1.5rem]">0.42</span></span>
       </MaskContainer>
     </div>
   );
